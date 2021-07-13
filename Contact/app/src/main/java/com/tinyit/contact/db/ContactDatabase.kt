5c69f113886f8cd6718b9d34f18728e064e99da3
Robert Eru <roberteru@Roberts-MacBook-Pro.local>
2021-07-13 20:49:24 +0100

    work well done

--- Contact/app/src/main/java/com/tinyit/contact/db/ContactDatabase.kt
+++ Contact/app/src/main/java/com/tinyit/contact/db/ContactDatabase.kt
@@ -28,7 +28,6 @@ abstract class ContactDatabase : RoomDatabase() {
                 )
                     .allowMainThreadQueries().build()
             }
-
             return INSTANCE
         }
 
